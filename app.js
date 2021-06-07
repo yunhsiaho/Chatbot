eaca1804364e75d0ae590b69986dc2ea96065e9d
hsia <yunhsia.ho@gmail.com>
2021-06-07 13:36:39 +0200

    test test

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ socket.on('message',addMessasge)
 
 function addMessages(message){
     $("#messages").append(
-        `<div class="bg-info p-3 m-3 col-8 text-light border rounded"><h4>${message.name}</h4> <p>${message.message}</p></div> `
+        `<div class="bg-info p-3 m-3 col-10 text-light border rounded"><h4>${message.name}</h4> <p>${message.message}</p></div> `
         )
 }
 
